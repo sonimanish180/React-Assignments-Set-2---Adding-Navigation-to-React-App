cf063ff07eef72c1df02d0669dad984a5b2fae1c
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-12-18 21:07:03 +0530

    chnagesc3

--- src/components/LocationDisplay.jsx
+++ src/components/LocationDisplay.jsx
@@ -3,13 +3,10 @@ import { useLocation } from "react-router-dom";
 
 export default function LocationDisplay() {
   let location = useLocation();
-  // const path = this.props.path;
-  // console.log(this.props.path);
+
   return (
     <>
       <div data-testid="location-display">{location.pathname}</div>
-      {/* {this.props.paths === "home" && <div>{"/"}</div>}
-      {this.props.paths === "about" && <div>{"/about"}</div>} */}
     </>
   );
 }
